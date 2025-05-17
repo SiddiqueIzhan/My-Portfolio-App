f1be67f444ac42a59c3b4979ab0c195abb506c2b
SiddiqueIzhan <izhanvk@gmail.com>
2025-05-17 09:48:45 +0530

    changed-photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,25 +17,23 @@ const Home = ({ text, speed = 150 }) => {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col items-center justify-center text-center px-4 sm:px-6 md:px-12 relative overflow-hidden"
+      className="max-h-screen flex flex-col items-center justify-center text-center px-4 sm:px-6 md:px-12 relative overflow-hidden"
     >
       <img
         src="\cover-photo.jpg"
         alt="Cover Background"
         className="absolute inset-0 w-full h-full object-cover opacity-40 z-0"
       />
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-16 text-left mt-32 md:mt-0 md:gap-0">
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between text-left mt-64 md:mt-0 md:gap-16">
         {/* Left Section with Typing Animation */}
         <motion.div
-          className="w-[90vw] md:max-w-[535px] md:mr-12 px-4 md:p-0"
+          className="w-[90vw] md:max-w-[535px] max-h-1/2 md:mr-12 px-4 md:p-0"
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
           <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold">
-            <span
-              className="overflow-hidden whitespace-wrap md:whitespace-nowrap border-r-4 border-white pr-4"
-            >
+            <span className="overflow-hidden whitespace-wrap md:whitespace-nowrap border-r-4 border-white pr-4">
               {displayedText}
             </span>
           </h1>
@@ -49,15 +47,15 @@ const Home = ({ text, speed = 150 }) => {
 
         {/* Right Section with Sliding Image */}
         <motion.div
-          className="w-full md:w-1/2 flex justify-center md:ml-24"
+          className="w-full md:w-1/2 max-h-1/2 md:max-h-lvh flex justify-center overflow-hidden relative bottom-16 -left-4 md:static"
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
           <img
-            src="\Siddique Photo.png"
+            src="\Siddique-izhan-png.png"
             alt="Profile"
-            className="w-64 h-96 md:w-96 md:h-[576px] object-cover md:mt-40 transform -scale-x-100"
+            className="h-[600px] md:h-[850px] object-cover"
           />
         </motion.div>
       </div>
